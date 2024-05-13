5d7f553a99a1c035123e6ccbbacd9beac19d1002
Raz-N-oren <razoren2394@gmail.com>
2024-05-13 15:19:33 +0300

    trying to navigate

--- force-app/main/default/lwc/subscriberTableList/subscriberTableList.js
+++ force-app/main/default/lwc/subscriberTableList/subscriberTableList.js
@@ -1,10 +1,15 @@
-import { LightningElement ,wire , track } from 'lwc';
+import { LightningElement ,wire , track, api } from 'lwc';
 import getSubscribers from '@salesforce/apex/SubscriberController.getSubscribers';
-
+import { NavigationMixin } from 'lightning/navigation';
 
 const columns = [
-    { label: 'Name', fieldName: 'Name__c' },
-    { label: 'Phone', fieldName: 'Phone__c', type: 'phone' },
+    { label: 'Name', fieldName: 'Name', type: 'button',
+    typeAttributes: {
+        label: { fieldName: 'Name' },
+        name: 'viewSubscriber',
+        variant: 'base', 
+    }},
+    { label: 'Phone', fieldName: 'Phone__c', type: 'phone'},
     { label: 'Email', fieldName: 'Email__c', type: 'email' },
     { label: 'Status', fieldName: 'Status__c'},
     { label: 'Data Used', fieldName: 'Data_Used__c'},
@@ -14,7 +19,7 @@ const columns = [
 
 export default class SubscriberTableList extends LightningElement {
 
-   
+    @api recordId;
     @track subscribersToDisplay = [];
     @track subscribers = [];
 
@@ -28,8 +33,8 @@ export default class SubscriberTableList extends LightningElement {
         if (data) {
          this.subscribers= data;
          console.log('this.subscribers ' +this.subscribers);
-         this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'DISCONNECTED');
-         console.log('subscribersToDisplay ' +this.subscribersToDisplay);
+         this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'Disconnected');
+         console.log('subscribersToDisplay ' + JSON.stringify(this.subscribersToDisplay));
          console.log('subscribersToDisplay.length ' +this.subscribersToDisplay.length);
         this.error = undefined;
 
@@ -45,7 +50,35 @@ export default class SubscriberTableList extends LightningElement {
             this.subscribersToDisplay = this.subscribers;
         }
         else{
-            this.subscribersToDisplay  = this.subscribers.filter(elem => elem.fields.Status.value != 'DISCONNECTED');
+            this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'Disconnected');
+        }
+    }
+
+    navigateSubscriberRecord(event) {
+        // Retrieve the action and row details from the event
+        const action = event.detail.action;
+        const row = event.detail.row;
+
+        console.log('Action: ', JSON.stringify(action));
+        console.log('action.name: ', action.name);
+        console.log('row.Id: ', row.Id);
+    
+        // Switch case to handle different actions
+        switch (action.name) {
+            // If the action is to view the freelancer
+            case 'viewSubscriber':
+                // Navigate to the record page of the specified freelancer
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: row.Id,
+                        objectApiName: 'Subscriber__c',
+                        actionName: 'view'
+                    }
+                });
+                break;
+            default:
+                break;
         }
     }
 
