9bf1280278217db0caf1f16b4abfe558af30fee0
Raz-N-oren <razoren2394@gmail.com>
2024-05-14 09:47:48 +0300

    Added descriptions

--- force-app/main/default/lwc/subscriberTableList/subscriberTableList.js
+++ force-app/main/default/lwc/subscriberTableList/subscriberTableList.js
@@ -19,178 +19,224 @@ export default class SubscriberTableList extends LightningElement {
     @track subscribersToDisplay = [];
     @track subscribers = [];
     @track DisconnectedSubscribers = [];
-    @track recordsTosearch = [];
+    @track recordsToSearch = [];
     @track recordsToDisplay = [];
-
     @track searchTerm = '';
     @track pagedSubscribers = [];
     @track includeDisconnected = false;
-    //@track currentPage = 1;
-    pageNumber = 1;
-    //@track totalrecords = 0;
-    //@track pageSize = 10;
     @track sortDirection = 'asc';
     @track sortedBy;
-
-    totalRecords;
     @track totalPages;
+
+    pageNumber = 1;
+    totalRecords;
     columns = columns;
 
-
+    /* 
+    * Created By: Raz
+    * Params: { error, data } - The result of the wired Apex method call, containing error and data properties.
+    * Description: This function handles the logic for retrieving subscriber data using a wired Apex method.
+    */
     @wire(getSubscribers)
     wiredSubscribers({ error, data }) {
-        console.log('data ' + JSON.stringify(data));
-
+        // Check if data is returned successfully.
         if (data) {
-         this.subscribers= data;
-         console.log('this.subscribers ' +this.subscribers);
-         this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'Disconnected');
-         this.DisconnectedSubscribers =  this.subscribers.filter(elem => elem.Status__c == 'Disconnected');
-         console.log('subscribersToDisplay ' + JSON.stringify(this.subscribersToDisplay));
-         console.log('subscribersToDisplay.length ' +this.subscribersToDisplay.length);
-         //this.pageSubscribers();
-         this.totalRecords = this.subscribersToDisplay.length;
-         console.log(' this.totalRecords ' + this.totalRecords);
-         this.totalPages = Math.ceil(this.subscribersToDisplay.length/pageSize);  
-         this.paginationHelper();
-        this.error = undefined;
-
-     } else if (error) {
-         this.error = error;
-         this.subscribers = undefined;
-         console.log('error' + JSON.stringify(error));
-     }
-    }
-
-    handleIncludeDisconnected(event){
-        this.searchTerm = '';
-        if(event.target.checked){
-            //this.subscribersToDisplay = this.subscribers;
-            this.includeDisconnected = true;
-            this.totalRecords = this.subscribers.length;
-            this.totalPages = Math.ceil(this.subscribers.length/pageSize);  
-            this.paginationHelper();
-            //this.currentPage = 1;
-        }
-        else{
-            //this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'Disconnected');
-            this.includeDisconnected = false;
-            this.totalPages = Math.ceil(this.subscribersToDisplay.length/pageSize);  
+            // If data is available, assign it to the subscribers property.
+            this.subscribers = data;
+            // Filter out disconnected subscribers and assign the filtered list to subscribersToDisplay.
+            this.subscribersToDisplay = this.subscribers.filter(sub => sub.Status__c != 'Disconnected');
+            // Filter only disconnected subscribers and assign the filtered list to DisconnectedSubscribers.
+            this.DisconnectedSubscribers = this.subscribers.filter(sub => sub.Status__c == 'Disconnected');
+            // Set the total number of records for pagination.
             this.totalRecords = this.subscribersToDisplay.length;
+            // Calculate the total number of pages based on the number of records and page size.
+            this.totalPages = Math.ceil(this.subscribersToDisplay.length / pageSize);  
+            // Perform pagination setup.
             this.paginationHelper();
-            //this.currentPage = 1;
+            // Reset any previous error.
+            this.error = undefined;
+        } 
+        // Check if there's an error
+        else if (error) {
+            // If there's an error, assign it to the error property
+            this.error = error;
+            // Reset subscribers and subscribersToDisplay.
+            this.subscribers = undefined;
+            this.subscribersToDisplay = undefined;
+            console.log('error' + JSON.stringify(error));
         }
     }
 
-    /*handleLoadMore() {
-        this.currentPage += 1;
-        this.pageSize += 10;
-        this.pageSubscribers();
-    }*/
+    /* 
+    * Created By: Raz.
+    * Params: event - The event object representing the checkbox state change.
+    * Description: This function handles the action triggered when including or excluding disconnected subscribers.
+    */
+    handleIncludeDisconnected(event) {
+        // Reset the search term
+        this.searchTerm = '';
+        // Check if the checkbox is checked.
+        if (event.target.checked) {
+            // If checked, include disconnected subscribers.
+            this.includeDisconnected = true;
+            // Update total records and total pages for pagination.
+            this.totalRecords = this.subscribers.length;
+            this.totalPages = Math.ceil(this.subscribers.length / pageSize);
+            // Perform pagination setup.
+            this.paginationHelper();
+        } else {
+            // If unchecked, exclude disconnected subscribers.
+            this.includeDisconnected = false;
+            // Update total pages and total records for pagination based on displayed subscribers.
+            this.totalPages = Math.ceil(this.subscribersToDisplay.length / pageSize);
+            this.totalRecords = this.subscribersToDisplay.length;
+            // Perform pagination setup.
+            this.paginationHelper();
+        }
+    }
 
+
+    /* 
+    * Created By: Raz.
+    * Params: event - The event object containing sorting requirement.
+    * Description: This function handles the action triggered when sorting subscriber data.
+    */
     handleSort(event) {
+        // Update sort direction and sorted by field based on event details
         this.sortDirection = event.detail.sortDirection;
         this.sortedBy = event.detail.fieldName;
+        // Sort the displayed subscribers based on the new sorting criteria.
         this.subscribersToDisplay = this.sortData(event.detail.fieldName, event.detail.sortDirection);
+        // Update the displayed subscribers after sorting
         this.pageSubscribers();
     }
 
+    /* 
+    * Created By: Raz.
+    * Params: fieldName - The name of the field by which the data should be sorted.
+    *         direction - The direction of sorting.
+    * Description: This function sorts the subscriber data based on the provided field name and sorting direction.
+    */
     sortData(fieldName, direction) {
+        // Deep clone the subscribers data to avoid mutating the original data.
         let parseData = JSON.parse(JSON.stringify(this.subscribersToDisplay));
+        
+        // Define a function to extract the value of the specified field from each record
         let keyValue = (a) => {
             return a[fieldName];
         };
+
+        // Determine whether to sort in ascending or descending order.
         let isReverse = direction === 'asc' ? 1 : -1;
+
+        // Sort the data based on the specified field and direction.
         parseData.sort((x, y) => {
+            // Get the values of the specified field from the current and next records.
             x = keyValue(x) ? keyValue(x) : ''; 
             y = keyValue(y) ? keyValue(y) : '';
+            // Compare the values and return the result based on the sorting direction.
             return isReverse * ((x > y) - (y > x));
         });
+
+        // Return the sorted data.
         return parseData;
     }
 
 
-    /*pageSubscribers() {
-        const startIndex = (this.currentPage - 1) * this.pageSize;
-        const endIndex = Math.min(startIndex + this.pageSize, this.totalRecords);
-        this.pagedSubscribers = this.subscribersToDisplay.slice(startIndex, endIndex);
-    }*/
-
+    /* 
+    * Created By: Raz.
+    * Params: event - The event object representing the search input change.
+    * Description: This function handles the action triggered when searching for subscribers.
+    */
     handleSearch(event) {
+        // Reset page number to 1 for pagination
+        this.pageNumber = 1;
+
+        // Check if search input has a value
         if(event.target.value.length > 0){
+            // If there is a search term, set it and filter subscribers based on the search term
             this.searchTerm = event.target.value.toLowerCase();
-            this.currentPage = 1;
-            this.recordsToDisplay = this.subscribers.filter(elem => elem.Name.toLowerCase().includes(this.searchTerm));
-            /*if(this.includeDisconnected == false){
-                this.subscribersToDisplay  = this.recordsTosearch.filter(elem => elem.Status__c != 'Disconnected');
-            }*/
+            this.recordsToDisplay = this.subscribers.filter(sub => sub.Name.toLowerCase().includes(this.searchTerm));
         }
+        // If search input is empty.
         else{
-            //this.recordsTosearch = [];
             if(this.includeDisconnected == false){
-                this.subscribersToDisplay  = this.subscribers.filter(elem => elem.Status__c != 'Disconnected');
+                // If includeDisconnected is false, filter out disconnected subscribers
+                this.subscribersToDisplay  = this.subscribers.filter(sub => sub.Status__c != 'Disconnected');
             }
             else{
+                // If includeDisconnected is true, display all subscribers
                 this.subscribersToDisplay  = this.subscribers;
             }
+            // Perform pagination setup
             this.paginationHelper();
         }
-        //this.pageSubscribers();
-
     }
 
+    /* 
+    * Created By: Raz.
+    * Params: -
+    * Description: This function handles the action triggered when the "Next Page" button is clicked.
+    */
     onNextPageClicked() {
+        // Reset search term to empty string
         this.searchTerm = '';
+        // Increment page number
         this.pageNumber = this.pageNumber + 1;
+        // Perform pagination setup
         this.paginationHelper();
     }
 
+    /* 
+    * Created By: Raz.
+    * Params: -
+    * Description: This function handles the action triggered when the "Previous Page" button is clicked.
+    */
     onPreviousPageClicked() {
+        // Reset search term to empty string.
         this.searchTerm = '';
+        // Decrement page number.
         this.pageNumber = this.pageNumber - 1;
+        // Perform pagination setup.
         this.paginationHelper();
     }
 
+    /* 
+    * Created By: Raz.
+    * Params: -
+    * Description: This function helps manage pagination by updating the records to display on the current page.
+    */
     paginationHelper() {
-        console.log('enter');
+        // Initialize the records to display array.
         this.recordsToDisplay = [];
-        // calculate total pages
-        // set page number 
+
+        // Ensure pageNumber stays within valid bounds.
         if (this.pageNumber <= 1) {
             this.pageNumber = 1;
-        } else if (this.pageNumber >= this.totalPages) {
+        } 
+        else if (this.pageNumber >= this.totalPages) {
             this.pageNumber = this.totalPages;
         }
 
-        // set records to display on current page 
+        // Loop through the data to determine records to display on the current page.
         for (let i = (this.pageNumber - 1) * pageSize; i < this.pageNumber * pageSize; i++) {
-            console.log('loop');
+            // Break loop if the total records have been reached.
             if (i === this.totalRecords) {
                 break;
             }
+            // Push records based on includeDisconnected flag.
             if(this.includeDisconnected == true){
-                /*if(this.searchTerm != ''){
-                    //this.recordsToDisplay.push(this.recordsTosearch[i]);
-                    this.recordsToDisplay.push(this.DisconnectedSubscribers[i]);
-                }
-                else{
-                   */
-                    this.recordsToDisplay.push(this.subscribers[i]);
-                //}
+                // If includeDisconnected is true, push all subscribers.
+                this.recordsToDisplay.push(this.subscribers[i]);
             }
-
             else{
-                /*if(this.searchTerm != ''){
-                    console.log('this.searchTerm 1');
-                    this.recordsToDisplay.push(this.recordsTosearch[i]);
-                    console.log('this.recordsTosearch[i] 1');
-                }*/
+                // If includeDisconnected is false, push only non-disconnected subscribers.
                 this.recordsToDisplay.push(this.subscribersToDisplay[i]);
             }
         }
     }
 
-    
+    // --------------- Getters ----------------
     get disablePreviousPageButtonGetter() {
         return this.pageNumber == 1;
     }
